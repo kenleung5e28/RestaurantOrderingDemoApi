ce3ab556907d59c727f2a5a164bcd79763af7c64
Ken Leung <kenleung5e28@gmail.com>
2022-10-17 01:44:26 +0800

    add seeding

--- Models/RestaurantContext.cs
+++ Models/RestaurantContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Xml.Linq;
 
 namespace RestaurantOrderingDemoApi.Models
 {
@@ -11,6 +12,23 @@ namespace RestaurantOrderingDemoApi.Models
                 "Server=127.0.0.1;Port=5432;Database=restaurant_demo;" +
                 "User Id=postgres;Password=postgres;"
             );
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Category>().HasData(
+                new Category { CategoryId = -1, Name = "Drinks" },
+                new Category { CategoryId = -2, Name = "Main course" });
+            modelBuilder.Entity<Item>().HasData(
+                new { ItemId = -1, Name = "Rice", Type = ItemType.Food, CategoryId = -2 },
+                new { ItemId = -2, Name = "Cheese burger", Type = ItemType.Food, CategoryId = -2 },
+                new { ItemId = -100, Name = "Iced lemon tea", Type = ItemType.Drink, CategoryId = -1 });
+            modelBuilder.Entity<Menu>().HasData(
+                new Menu { 
+                    MenuId = -1, Name = "A la carte", 
+                    AvailableOnHolidays = true, AvailableOnWeekdays = true,
+                });
         }
 
         public DbSet<Category> Categories { get; set; }

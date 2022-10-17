4221c40b79e900bd2d3301d78ca84a0ef85045cc
Ken Leung <kenleung5e28@gmail.com>
2022-10-17 10:30:59 +0800

    fix seed data

--- Models/RestaurantContext.cs
+++ Models/RestaurantContext.cs
@@ -25,10 +25,19 @@ namespace RestaurantOrderingDemoApi.Models
                 new { ItemId = -2, Name = "Cheese burger", Type = ItemType.Food, CategoryId = -2 },
                 new { ItemId = -100, Name = "Iced lemon tea", Type = ItemType.Drink, CategoryId = -1 });
             modelBuilder.Entity<Menu>().HasData(
-                new Menu { 
-                    MenuId = -1, Name = "A la carte", 
-                    AvailableOnHolidays = true, AvailableOnWeekdays = true,
+                new Menu
+                {
+                    MenuId = -1,
+                    Name = "A la carte",
+                    AvailableOnHolidays = true,
+                    AvailableOnWeekdays = true,
                 });
+            modelBuilder.Entity<Menu>()
+                .HasMany(menu => menu.Items)
+                .WithMany(item => item.Menus)
+                .UsingEntity(j => j.ToTable("ItemMenu").HasData(
+                    new { MenusMenuId = -1, ItemsItemId = -1 },
+                    new { MenusMenuId = -1, ItemsItemId = -2 }));
         }
 
         public DbSet<Category> Categories { get; set; }

66c14c6661f3219598ae249ec1f383712248e0bc
Ken Leung <kenleung5e28@gmail.com>
2022-10-17 14:43:46 +0800

    add demo route

--- Controllers/MenuController.cs
+++ Controllers/MenuController.cs
@@ -14,9 +14,24 @@ public class MenuController : ControllerBase
         _logger = logger;
     }
 
-    [HttpGet(Name = "GetMenu")]
-    public Menu Get(int menuId)
+    public record MenuDto
     {
-        return new Menu { MenuId = menuId, Name = "dummy", Items = new List<Item>()};
+        public string Name { get; set; }
+        public List<Item>? Items { get; set; }
+        public List<Combo>? Combos { get; set; }
+    }
+
+    [HttpGet]
+    public MenuDto? Get(int menuId)
+    {
+        using var context = new RestaurantContext();
+        return context.Menus
+            .Where(menu => menu.MenuId == menuId)
+            .Select(menu => new MenuDto
+            {
+                Name = menu.Name,
+                Items = menu.Items.ToList(),
+                Combos = menu.Combos.ToList(),
+            }).FirstOrDefault();
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,8 +1,13 @@
+using Microsoft.AspNetCore.Mvc.Formatters;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(configure =>
+{
+    configure.OutputFormatters.RemoveType<HttpNoContentOutputFormatter>();
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
